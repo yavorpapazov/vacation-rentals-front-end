020fad03ed683713fb38cd265155088b8f57c848
Yavor Papazov <y_papaz@yahoo.com>
2023-05-21 14:26:49 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,73 +1,114 @@
 import { useState, useEffect } from 'react'
-import People from './People'
+import Bnbs from './Bnbs'
 
 function App() {
-  let [ people, setPeople ] = useState([])
-  let [ name, setName ] = useState('')
-  let [ age, setAge ] = useState('')
+  let [bnbTitle, setBnbTitle] = useState('')
+  let [bnbCity, setBnbCity] = useState('')
+  let [bnbCountry, setBnbCountry] = useState('')
+  let [bnbCost, setBnbCost] = useState('')
+  let [imageInput, setImageInput] = useState('')
+  let [imageFile, setImageFile] = useState('')
+  let [bnbs, setBnbs] = useState([])
 
-  function handleName(e) {
-    setName(e.target.value)
+  function handleTitle(e) {
+    setBnbTitle(e.target.value)
   }
-  function handleAge(e) {
-    setAge(e.target.value)
+  function handleCity(e) {
+    setBnbCity(e.target.value)
+  }
+  function handleCountry(e) {
+    setBnbCountry(e.target.value)
+  }
+  function handleCost(e) {
+    setBnbCost(e.target.value)
+  }
+  function handleImage(e) {
+    setImageInput(e.target.value)
+    const reader = new FileReader()
+    reader.readAsDataURL(e.target.files[0])
+    reader.onload = () => {
+      setImageFile(reader.result)
+    }
   }
   async function handleSubmit(e) {
     e.preventDefault()
     let userInputObj = {
-      name,
-      age
+      bnbTitle,
+      bnbCity,
+      bnbCountry,
+      bnbCost,
+      bnbImage: imageFile
     }
-    setName('')
-    setAge('')
-    let response = await fetch('http://localhost:5000/api/people/', {
+    setBnbTitle('')
+    setBnbCity('')
+    setBnbCountry('')
+    setBnbCost('')
+    setImageInput('')
+    let response = await fetch('http://localhost:5000/bnbs', {
       method: 'POST',
       mode: "cors",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*"
       },
       body: JSON.stringify(userInputObj)
     })
     let receivedData = await response.json()
-    alert(`${receivedData.name} has been added to DB`)
+    alert(`${receivedData.bnbCity} has been added to DB`)
   }
-  async function handleDetails(itemId) {
-    let response = await fetch(`http://localhost:5000/api/people/${itemId}`)
-    let receivedData = await response.json()
-    alert(`${receivedData[0].name}, ID: ${receivedData[0]._id}`)
-  }
-  async function handleRemove(itemId) {
-    let response = await fetch(`http://localhost:5000/api/people/${itemId}`, {
-      method: 'DELETE',
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    let receivedData = await response.json()
-    alert(`${receivedData.name} has been removed from DB`)
-  }
+  // async function handleDetails(itemId) {
+  //   let response = await fetch(`http://localhost:5000/api/people/${itemId}`)
+  //   let receivedData = await response.json()
+  //   alert(`${receivedData[0].name}, ID: ${receivedData[0]._id}`)
+  // }
+  // async function handleRemove(itemId) {
+  //   let response = await fetch(`http://localhost:5000/api/people/${itemId}`, {
+  //     method: 'DELETE',
+  //     mode: "cors",
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     }
+  //   })
+  //   let receivedData = await response.json()
+  //   alert(`${receivedData.name} has been removed from DB`)
+  // }
   useEffect(() => {
     async function getData() {
-      let response = await fetch('http://localhost:5000/api/people/')
+      let response = await fetch('http://localhost:5000/bnbs/')
       let receivedData = await response.json()
-      setPeople(receivedData)
+      setBnbs(receivedData)
     }
     getData()
   }, [])
-  let result = people.map(item => <People key={item._id} {...item} details={handleDetails} remove={handleRemove} />)
+  let result = bnbs.map(item => <Bnbs key={item._id} {...item} />)
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="name">Name:</label>
+          <label htmlFor="title">Title:</label>
           <br />
-          <input id="name" type="text" value={name} onChange={handleName} required></input>
+          <input id="title" type="text" value={bnbTitle} onChange={handleTitle} required></input>
         </div>
         <div>
-          <label htmlFor="age">Age:</label>
+          <label htmlFor="city">Location:</label>
           <br />
-          <input id="age" type="number" value={age} onChange={handleAge} required></input>
+          <input id="city" type="text" value={bnbCity} onChange={handleCity} required></input>
+        </div>
+        <div>
+          <label htmlFor="country">Country:</label>
+          <br />
+          <input id="country" type="text" value={bnbCountry} onChange={handleCountry} required></input>
+        </div>
+        <div>
+          <label htmlFor="cost">Cost:</label>
+          <br />
+          <input id="cost" type="number" value={bnbCost} onChange={handleCost} required></input>
+        </div>
+        <div>
+          <label htmlFor="image">Select image:</label>
+          <br />
+          <input accept='image/' id="image" type="file" value={imageInput} onChange={handleImage} required></input>
         </div>
         <div>
           <button>Add</button>
---
+++ src/Bnbs.js
@@ -0,0 +1,10 @@
+function Bnbs({ bnbImage, bnbTitle }) {
+    return (
+        <div>
+            <img src={bnbImage} alt={bnbTitle} />
+            <br />
+        </div>
+    )
+}
+
+export default Bnbs
